d646c38ef8cf92e8977c4a674475da3f7eca0a9c
prodanalytics <prodanalytics@tabaterra.az>
2024-06-25 13:36:30 +0400

    ReadRepository

---
+++ Core/OnionArchAPI.Application/Abstractions/Repositorys/IReadRepository.cs
@@ -0,0 +1,25 @@
+﻿using OnionArchAPI.Domen.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchAPI.Application.Abstractions.Repositorys
+{
+    public interface IReadRepository<T> : IRepository<T> where T : class, IEntityBase
+    {
+        IQueryable<T> GetAll(bool tracing=false);
+
+   
+
+        Task<List<T>> GetPaginationAsync(int page, int size, bool tracing = false);
+
+        IQueryable<T> GetWhere(Expression<Func<T, bool>> predicate, bool tracing = false);
+
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate, bool tracing = false);
+
+
+    }
+}
---
+++ Core/OnionArchAPI.Application/Abstractions/Repositorys/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchAPI.Domen.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchAPI.Application.Abstractions.Repositorys
+{
+    public interface IRepository<T> where T : class,IEntityBase
+    {
+        public DbSet<T> Table { get;  }
+    }
+}
