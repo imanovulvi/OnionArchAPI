ded00c123d755f16c4ac68351ab31218194b7566
prodanalytics <prodanalytics@tabaterra.az>
2024-06-25 11:10:15 +0400

    Create Entity

---
+++ Core/OnionArchAPI.Domen/Common/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchAPI.Domen.Common
+{
+    public class EntityBase:IEntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }=DateTime.UtcNow;
+        public bool IsDelete { get; set; }=false;
+    }
+}
