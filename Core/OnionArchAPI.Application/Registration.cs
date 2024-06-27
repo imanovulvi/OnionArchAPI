fdcf30827452ae08aa61bbe34f403a7c3d97a0be
prodanalytics <prodanalytics@tabaterra.az>
2024-06-27 13:16:21 +0400

    Entitys toles

--- Core/OnionArchAPI.Application/Registration.cs
+++ Core/OnionArchAPI.Application/Registration.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OnionArchAPI.Application.Base;
 using OnionArchAPI.Application.Exception;
+using OnionArchAPI.Application.Features.Product.Rules;
 using System.Reflection;
 
 namespace OnionArchAPI.Application
@@ -11,6 +13,19 @@ namespace OnionArchAPI.Application
             services.AddMediatR(x=>x.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
             services.AddTransient<ExceptionsMiddleWare>();
 
+            services.AddRulesFromAssemblies();
+
         }
+
+        public static void AddRulesFromAssemblies(this IServiceCollection services) 
+        {
+            var assembly=Assembly.GetExecutingAssembly();
+            foreach (var item in assembly.GetTypes())
+                if (item.IsClass && item.IsSubclassOf(typeof(BaseRules))&& item!=typeof(BaseRules))
+                    services.AddTransient(item);
+
+
+        }
+
     }
 }
